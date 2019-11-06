2f6e63ed24e80f9e40ee82ab0b09255e7d1dd9cf
burbaljaka <kapitonov.timur@gmail.com>
2019-11-06 16:16:38 +0300

    почти доделал

--- News/scrap/models.py
+++ News/scrap/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Post(models.Model):
-    post_title = models.CharField(unique=True)
-    post_subtitle = models.CharField()
-    post_content = models.CharField()
+    post_title = models.CharField(unique=True, max_length = 100)
+    post_subtitle = models.CharField(max_length = 100)
+    post_content = models.CharField(max_length = 2000)
     post_date = models.DateField()
+    post_link = models.CharField(max_length = 300)
 
     def __str__(self):
         return self.post_title
--- News/scrap/views.py
+++ News/scrap/views.py
@@ -4,12 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-def get_news(date):
-
-
 def base(request):
-
-    check_date = Post.objects.latest('post_date')
 
     page = requests.get('https://pasmi.ru/cat/news/')
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -23,12 +18,17 @@ def base(request):
         post_date_time = datetime.datetime.strptime(post_date, '%d.%m.%Y / %H:%M')
         post_title = article.find('h1').get_text()
         post_link = article.find('a')['href']
-
-        if post_date_time > check_date.post_date or check_date == NULL:
-            posts.append(Post(post_date = post_date_time, post_title = post_title, post_link = post_link))
-
-        elif post_date_time < check_date.post_date:
+    try:
+        check_date = Post.objects.latest('post_date')
+    except:
+        pass
+    else:
+        if post_date_time < check_date[0].post_date:
             break
+
+    posts.append(Post(post_date = post_date_time, post_title = post_title, post_link = post_link))
+
+
         # print("заголовок", post_title)
         # print("дата", post_date)
         # print("ссылка", post_link)
